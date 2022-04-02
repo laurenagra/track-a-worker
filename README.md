453af6e59f61821b0c2d91d23598bf6ce0ed1703
Lauren A <laurenhaileyama@gmail.com>
2022-04-02 12:55:45 -0400

    Add env file, finish server code, update readme

--- README.md
+++ README.md
@@ -1,1 +1,68 @@
 # track-a-worker
+
+## Table of Contents
+- [General](#general)
+- [User Story](#general)
+- [Installation](#installation)
+- [Tech](#tech)
+- [Screenshots](#screenshots)
+- [Acceptance Criteria](#acceptance-criteria)
+- [Authors](#authors)
+
+## General
+
+## User Story 
+```md
+AS A business owner
+I WANT to be able to view and manage the departments, roles, and employees in my company
+SO THAT I can organize and plan my business
+```
+## Installation
+run the following commands to install the required packages
+
+Node: `npm init -y`
+
+`npm i`
+
+MySql: `npm i mysql2`
+
+Inquirer: `npm i inquirer`
+
+```Initiate the application through the command line with the command node server.js. You will be taken through a series of prompts that will allow you to view, add, and update various information in the database. When given a list of choices, these choices are pulled from the database to accurately reflect any changes you may have made.``` 
+
+## Tech
+- MySQL
+- Node.js
+- Javascript 
+
+## Screenshots
+
+## Acceptance Criteria 
+```md
+GIVEN a command-line application that accepts user input
+WHEN I start the application
+THEN I am presented with the following options: view all departments, view all roles, view all employees, add a department, add a role, add an employee, and update an employee role
+WHEN I choose to view all departments
+THEN I am presented with a formatted table showing department names and department ids
+WHEN I choose to view all roles
+THEN I am presented with the job title, role id, the department that role belongs to, and the salary for that role
+WHEN I choose to view all employees
+THEN I am presented with a formatted table showing employee data, including employee ids, first names, last names, job titles, departments, salaries, and managers that the employees report to
+WHEN I choose to add a department
+THEN I am prompted to enter the name of the department and that department is added to the database
+WHEN I choose to add a role
+THEN I am prompted to enter the name, salary, and department for the role and that role is added to the database
+WHEN I choose to add an employee
+THEN I am prompted to enter the employee’s first name, last name, role, and manager, and that employee is added to the database
+WHEN I choose to update an employee role
+THEN I am prompted to select an employee to update and their new role and this information is updated in the database 
+```
+
+## License 
+MIT 
+
+## Authors 
+**Lauren Agra** 
+- [LinkedIn](https://www.linkedin.com/in/lauren-agra-aa868b1b8/)
+- [Github](https://github.com/laurenagra)
+- [Portfolio](https://laurenagra.github.io/Portfolio-Base/)
--- server.js
+++ server.js
@@ -4,36 +4,283 @@ const SqlQueries = require("./db/sqlqueries");
 const sql = new SqlQueries();
 
 const initDb = () => {
-    db.connect((err) => {
-        if (err) throw err;
-        console.log("Connected to database!");
-        console.log("Welcome to the Employee Management App");
-    });
+  db.connect((err) => {
+    if (err) throw err;
+    console.log("Connected to database!");
+    console.log("Welcome to the Employee Management App");
+  });
 };
 
 initDb();
 
-const promptUser = () => { 
+const promptUser = () => {
+  inquirer
+    .prompt([
+      {
+        type: "checkbox",
+        name: "choices",
+        message: "What would you like to do?",
+        choices: [
+          "View all employees",
+          "Add employee",
+          "Update employee roll",
+          "View all roles",
+          "Add role",
+          "View all departments",
+          "Add department",
+          "Finished",
+        ],
+      },
+    ])
+    .then((response) => {
+      const choice = response.choices[0];
+
+      choice.indexOf("View") != -1
+        ? viewTable(choice)
+        : choice === "Add employee"
+        ? addEmployee()
+        : choice === "Update employee role"
+        ? updateRole()
+        : choice === "Add role"
+        ? addRole()
+        : choice === "Add department"
+        ? addDepartment()
+        : choice === "Finished"
+        ? terminateApp()
+        : console.log("error");
+    });
+};
+viewTable = (choice) => {
+  let viewQuery = "";
+  if (choice === "View all employees") {
+    viewQuery = sql.viewEmployees();
+  } else if (choice === "view all roles") {
+    viewQuery = sql.viewRoles();
+  } else {
+    viewQuery = sql.viewDepartments();
+  }
+  if (viewQuery === " ") throw new error("Did not provide valid input.");
+
+  db.query(viewQuery, (err, results) => {
+    if (err) throw err;
+    console.log("");
+    console.table(results);
+    promptUser();
+  });
+};
+
+function addEmployee() {
+  let obj = {};
+  let roleNames = [];
+  db.query(sql.roleNames(), (err, results) => {
+    if (err) throw err;
+    results.forEach((role) => {
+      roleNames.push({
+        name: role.title,
+        value: role.id,
+      });
+    });
+    console.log(roleNames);
     inquirer
-        .prompt([
-            {
-                type: "checkbox",
-                name: "choices",
-                message: "What would you like to do?",
-                choices: [
-                    "View all employees",
-                    "Add employee",
-                    "Update employee roll",
-                    "View all roles",
-                    "Add role",
-                    "View all departments",
-                    "Add department",
-                    "Finished",
-                ],
-            },
-        ])
-        .then((response) => {
-            const choice = response.choices[0];
-            
-        })
+      .prompt([
+        {
+          type: "input",
+          name: "first",
+          message: "What is the employee's first name?",
+        },
+        {
+          type: "input",
+          name: "last",
+          message: "What is their last name?",
+        },
+        {
+          type: "Checkbox",
+          name: "role",
+          message: "What is their role?",
+          choices: roleNames,
+        },
+      ])
+      .then((data) => {
+        obj.firstName = data.first;
+        obj.lastName = data.last;
+        obj.role = data.role;
+        let viewManagers = [];
+        db.query(sql.getManager(), (err, results) => {
+          if (err) throw err;
+          results.forEach((manager) => {
+            viewManagers.push({
+              name: manager.first_name + " " + manager.last_name,
+              value: manager.id,
+            });
+          });
+          viewManagers.push({ name: "None", value: null });
+
+          inquirer
+            .prompt([
+              {
+                type: "list",
+                name: "manager",
+                message: "who is the manager?",
+                choices: viewManagers,
+              },
+            ])
+            .then((data) => {
+              obj.manager = data.manager;
+
+              db.query(
+                sql.addEmployee(
+                  obj.firstName,
+                  obj.lastName,
+                  obj.role,
+                  obj.manager
+                ),
+                (err, results) => {
+                  if (err) throw err;
+                  console.log("Added new employee to database");
+                }
+              );
+            });
+        });
+      });
+  });
 }
+
+addRole = () => {
+  let roleObj = {};
+  db.query(sql.viewDepartments(), (err, results) => {
+    if (err) throw err;
+    let viewDepartment = [];
+    results.forEach((dept) => {
+      viewDepartment.push({ name: dept.dept_name, value: dept.id });
+    });
+    inquirer
+      .prompt([
+        {
+          type: "list",
+          name: "department",
+          message: "Which department does this role belong to?",
+          choices: viewDepartment,
+        },
+      ])
+      .then((deptChoice) => {
+        roleObj.deptId = deptChoice.department;
+
+        inquirer
+          .prompt([
+            {
+              type: "input",
+              name: "title",
+              message: "What role would you like to add?",
+            },
+            {
+              type: "input",
+              name: "salary",
+              message: "What is the expected salary for this role?",
+            },
+            {
+              type: "confirm",
+              name: "isManagement",
+              message: "Is this a management position?",
+            },
+          ])
+          .then((data) => {
+            roleObj.title = data.title;
+            roleObj.salary = data.salary;
+            roleObj.isManagement = data.isManagement;
+            db.query(
+              sql.addRole(
+                roleObj.title,
+                roleObj.salary,
+                roleObj.deptId,
+                roleObj.isManagement
+              ),
+              (err, results) => {
+                if (err) throw err;
+                console.log("Added role to the database.");
+                promptUser();
+              }
+            );
+          });
+      });
+  });
+};
+
+addDepartment = () => {
+  inquirer
+    .prompt([
+      {
+        type: "input",
+        name: "department",
+        message: "what is the name of the department?",
+      },
+    ])
+    .then((data) => {
+      db.query(sql.addDepartment(data.department), (err, results) => {
+        if (err) throw err;
+        console.log("Added department to database");
+        promptUser();
+      });
+    });
+};
+
+updateRole = () => {
+  let updatedData = {};
+  let employees = [];
+  let roleNames = [];
+  db.query(sql.viewEmployeeTable(), (err, results) => {
+    if (err) throw err;
+    results.forEach((employee) => {
+      employees.push({
+        name: employee.first_name + " " + employee.last_name,
+        value: employee.id,
+      });
+    });
+    inquirer
+      .prompt([
+        {
+          type: "list",
+          name: "employee",
+          message: "Which employee's role would you like to update?",
+          choices: employees,
+        },
+      ])
+      .then((data) => {
+        updatedData.employee = data.employee;
+        db.query(sql.roleNames(), (err, results) => {
+          if (err) throw err;
+          results.forEach((role) => {
+            roleNames.push({
+              name: role.title,
+              value: role.id,
+            });
+          });
+          inquirer
+            .prompt([
+              {
+                type: "list",
+                name: "role",
+                message:
+                  "Which role would you like to assign to this employee?",
+                choices: roleNames,
+              },
+            ])
+            .then((data) => {
+              updatedData.role = data.role;
+              db.query(
+                sql.updateEmployee(updatedData.employee, updatedData.role),
+                (err, results) => {
+                  if (err) throw err;
+                  console.log("Employee's role has been updated");
+                  promptUser();
+                }
+              );
+            });
+        });
+      });
+  });
+};
+
+terminateApp = () => {
+  console.log("Thank you for visiting");
+};
+promptUser();
